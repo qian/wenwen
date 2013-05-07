4839791ac9f075e2ddb612682c4401d0e64c38f9
qian <qiananchuan@gmail.com>
2013-05-07 13:19:36 +0800

    add

---
+++ data/sitemap.js
@@ -0,0 +1,8 @@
+﻿var sitemap = 
+
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k),_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q)])]);}; 
+var b="rootNodes",c="pageName",d="方案A：通栏",e="type",f="Wireframe",g="url",h="方案A：通栏.html",i="children",j="方案A+顶部配色1",k="方案A_顶部配色1.html",l="方案A+顶部配色2",m="方案A_顶部配色2.html",n="方案A:头部扩展",o="方案A_头部扩展.html",p="方案B：左右结构",q="方案B：左右结构.html";
+return _creator();
+})();
