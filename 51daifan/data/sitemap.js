438608adb03e860db60f614f7125d6e62e4991bb
qian <qiananchuan@gmail.com>
2013-05-25 11:35:43 +0800

    remove

--- 51daifan/data/sitemap.js
+++
@@ -1,8 +0,0 @@
-﻿var sitemap = 
-
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k),_(c,l,e,f,g,m),_(c,n,e,f,g,o)]),_(c,p,e,f,g,q,i,[_(c,l,e,f,g,r),_(c,s,e,f,g,t),_(c,u,e,f,g,v),_(c,w,e,f,g,x)])]);}; 
-var b="rootNodes",c="pageName",d="首页-我要带饭V1.0",e="type",f="Wireframe",g="url",h="首页-我要带饭V1_0.html",i="children",j="注册",k="注册.html",l="我的订单",m="我的订单.html",n="我的厨房",o="我的厨房.html",p="首页-我要订餐",q="首页-我要订餐.html",r="我的订单_1.html",s="temp_首页",t="temp_首页.html",u="temp",v="temp.html",w="backup_v1.0",x="backup_v1_0.html";
-return _creator();
-})();
